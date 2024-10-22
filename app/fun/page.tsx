83be491aeb69cf4994734c4bf35f0f77bad4bcae
Marvin Chen <mc2971@princeton.edu>
2024-10-21 21:37:33 -0400

    changed text

--- app/fun/page.tsx
+++ app/fun/page.tsx
@@ -6,9 +6,8 @@ export default function Fun() {
       <h1 className="text-4xl font-bold mb-6">Fun Facts About Me</h1>
       <ul className="list-disc list-inside space-y-4">
         <li>I can speak three languages fluently: English, Mandarin, and Cantonese!</li>
-        <li>My favorite Pokémon is Charizard (which inspired my Pokémon Classifier project).</li>
-        <li>I'm an avid hiker and have climbed several peaks in the Appalachian Mountains.</li>
-        <li>I'm a huge fan of Korean dramas and use them to practice my Korean language skills.</li>
+        <li>My (arguably) favorite Pokémon is Dragonite, which inspired my Pokémon Classifier project.</li>
+        <li>I'm an avid hiker and have climbed several mountains in South Korea. I have also been to South Korea two summers in a row! </li>
         <li>In my free time, I enjoy reading tech blog posts about the latest trends in AI and machine learning. Got to see Fei-Fei Li in person, who inspired my interest in AI. </li>
       </ul>
     </div>
